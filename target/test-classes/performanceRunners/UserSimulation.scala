f4aba247c8ff7777432704d06c7297d761cb2d02
EMRE DOĞANGÜN <50294340+Emredogangun@users.noreply.github.com>
2021-12-21 12:29:52 +0300

    gatling

---
+++ target/test-classes/performanceRunners/UserSimulation.scala
@@ -0,0 +1,17 @@
+package performanceRunners
+
+import scala.concurrent.duration.{Duration, SECONDS}
+
+class UserSimulation extends Simulation{
+
+  val myFirstLoadTest = scenario("my task").exec(karateFeature("classpath:examples/meetup/meetup.feature"))
+
+  val protocol ={
+    karateProtocol();
+  }
+ setup(
+   myFirstLoadTest.inject(rampUsers(20) during(Duration(20,SECONDS))).protocols(protocol)
+ )
+}
+
+//mvn clean test-compile gatling:test (enter)
